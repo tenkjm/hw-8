c0918b97ccb34b8c1a3d98c277b7c1cfeedfff16
anton <atour07@yandex.ru>
2018-09-10 20:04:58 +0300

    functions was written need logic

--- hw-08/egg_python/segmentation.py
+++ hw-08/egg_python/segmentation.py
@@ -1,7 +1,8 @@
 import numpy as np
 import keras
+import math
 from keras.optimizers import SGD
-#import maxflow
+import maxflow
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Dropout, Activation
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
@@ -41,10 +42,41 @@ def train_unary_model(images, gt):
           verbose=1, validation_split=0.1)
     model.save("saved_weights.h5")
 
-    return {}
+    return model
 
 def segmentation(unary_model, images):
+    for i in range(0, 1):
+        im = images[i]
+        height, width, channels = im.shape
+
+        
     return [np.zeros(img.shape[:2]) for img in images]
+
+def segment_single(im, model):
+    
+    height, width, channels = im.shape
+    im_bg = np.zeros((height+12, width+12, channels))
+    im_bg = (im_bg + 1) * 255  
+
+    # Your work: Compute where it should be
+    pad_left = 6
+    pad_top = 6
+
+    im_bg[pad_top:pad_top + height,
+        pad_left:pad_left + width,
+        :] = im
+    g = maxflow.Graph[float]()
+    nodes = g.add_grid_nodes(im.shape)
+
+    for j in range(0, width-1):
+        for k in range(0, height-1):
+            prof = model.predict( im_bg[j:j+13, k:k+13])
+            g.add_grid_edges(nodes, phi_ij)
+            g.add_grid_tedges(nodes, phi_i1, phi_i0) 
+    g.maxflow()
+    graph = g.get_grid_segments(nodes)
+
+            
 
 def vgg10(weights_paty=None):
     model = Sequential()
@@ -67,3 +99,15 @@ def vgg10(weights_paty=None):
 
    
     return model
+
+def get_unary_potential(probability_tuple):
+    return -math.log(probability_tuple(0) if probability_tuple(0)>probability_tuple(1)  else probability_tuple(1))
+
+def get_binary_potential(yi, yj,classI, classJ):
+    A=0.5
+    B=0.5
+    sigma = 0.01
+    delta = 1 if classI==classJ else 0
+    psy = A+B*math.exp(-(np.linalg.dot(yi,yj)**2)/(2*sigma**2))
+    return (1-delta)*psy
+
